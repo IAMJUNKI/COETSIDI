7c6975961d8616a9b1b6f5c703114a97dfa617ef
Diego <djunquerasaez@gmail.com>
2024-06-04 00:39:22 +0200

    formulario avanzado, falta guardar la informacion en la base de datos y plantear la funcion estudiante internacional

--- client/dashboard/src/js/dashboard.js
+++ client/dashboard/src/js/dashboard.js
@@ -16,10 +16,11 @@ $(document).on('change', '#selector-grados', async function (event) {
 
     console.log($(event.currentTarget).val())
     const gradoElegido = $(event.currentTarget).val()
-    if(gradoElegido === 'grado_electrica_electronica'||gradoElegido === 'grado_diseno_mecanica' || gradoElegido === 'erasmus'){
+    if(gradoElegido === '56DM'||gradoElegido === '56EE' || gradoElegido === 'erasmus'){
         $( "#curso5" ).prop( "disabled", false );
     } else{
         $( "#curso5" ).prop( "disabled", true );
+        $( "#curso5" ).prop( "checked", false );
     }
     document.getElementById("segundaParteFormulario").style.display = "";
     
@@ -45,9 +46,9 @@ $(document).on('submit', '#js_form_buscar_asignaturas', async function (event) {
 
     event.preventDefault()
     if (preventDoubleClick(event)) { return };
-    console.log('somethng',event.target)
+    // console.log('somethng',event.target)
     const data = new FormData(event.target)
-    console.log(data)
+    // console.log(data)
     for (let [key, value] of data.entries()) {
         console.log(`${key}: ${value}`);
       }
@@ -60,10 +61,53 @@ $(document).on('submit', '#js_form_buscar_asignaturas', async function (event) {
         contentType: false,
         processData: false,
         success: function (objetoEstructurado) {
-           
+            $('#js_form_asignaturas_usuario').empty()
+            const selectorAsignaturas = document.createElement('div')
+
+           const arrayHtml = [
+                `<hr>
+                <h2 class="fs-5">Selecciona tus asignaturas</h2>`
+            ];
+        // console.log('huh',objetoEstructurado)
+  
+        Object.keys(objetoEstructurado).forEach(semestre => {
+
+            arrayHtml.push(`<h4 class="fs-5">Semestre ${semestre.split('_')[1]}</h4>`)
+          const semestreInfo = objetoEstructurado[semestre];
+        //   console.log(semestre.split('_')[1],'semestre')
+
+          Object.keys(semestreInfo).forEach(curso => {
+     
+            arrayHtml.push(`<h6>${curso} curso</h6>`)
+            const arrayDeAsignaturas = semestreInfo[curso];
+            console.log(curso,'curso')
+ 
+            arrayDeAsignaturas.forEach(item => {
+
+                const grupo = item.Grupo;
+                const asignatura = item.Asignatura;
+
+                arrayHtml.push(`
+                
+                <input type="checkbox" class="btn-check" id="${grupo}_${asignatura}" value="${grupo}_${asignatura}" autocomplete="off" name="asignaturasSeleccionadas">
+                <label class="btn btn-outline-primary" for="${grupo}_${asignatura}">${grupo}: ${asignatura}</label>
+               
+                `)
+              
+            //   console.log(`Grupo: ${grupo}, Asignatura: ${asignatura}`);
+            });
+          });
+        });
+        
+            arrayHtml.forEach(function(array) {
+                selectorAsignaturas.innerHTML += array;
+            });
+    
+            document.getElementById('js_form_asignaturas_usuario').append(selectorAsignaturas)  
             
         },
         error: function (error) {
+            $('#js_form_asignaturas_usuario').empty()
            console.error(error)
         }
     })
