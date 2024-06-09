a46124cb44c3e94962cb4dfe37773a4d084495e6
Diego <djunquerasaez@gmail.com>
2024-06-09 13:20:34 +0200

    google calendar mejorr

--- server/modules/googleCalendar/services.js
+++ server/modules/googleCalendar/services.js
@@ -1,81 +1,75 @@
 const fs = require('fs');
 const path = require('path');
 const process = require('process');
-const {authenticate} = require('@google-cloud/local-auth');
-const {google} = require('googleapis');
+const { OAuth2Client } = require('google-auth-library');
+const { google } = require('googleapis');
 const helpers = require('./helpers.js')
 const axios = require('axios');
-const { knex } = require('@db/knex.js')
+const { knex } = require('@db/knex.js');
+const express = require('express');
 
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events"]
 const TOKEN_PATH = path.join(process.cwd(), '../token.json');
 const CREDENTIALS_PATH = path.join(process.cwd(), '../credentials.json');
 
-async function loadSavedCredentialsIfExist() {
-  try {
-      const content = fs.readFileSync(TOKEN_PATH);
-      const credentials = JSON.parse(content);
-      return google.auth.fromJSON(credentials);
-  } catch (err) {
-      return null;
-  }
-}
-
-
-async function saveCredentials(client) {
-  const content = fs.readFileSync(CREDENTIALS_PATH);
-  const keys = JSON.parse(content);
-  const key = keys.installed || keys.web;
-  const payload = JSON.stringify({
-      type: 'authorized_user',
-      client_id: key.client_id,
-      client_secret: key.client_secret,
-      refresh_token: client.credentials.refresh_token,
-  });
-  fs.writeFileSync(TOKEN_PATH, payload);
-}
-
 async function authorize() {
-  let client = await loadSavedCredentialsIfExist();
-  if (client) {
-      return client;
-  }
-  client = await authenticate({
-      keyfilePath: CREDENTIALS_PATH,
-      scopes: SCOPES,
+  const credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH));
+  const { client_secret, client_id, redirect_uris } = credentials.installed || credentials.web;
+  const oAuth2Client = new OAuth2Client(client_id, client_secret, redirect_uris[0]);
+
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+    prompt: 'consent'
   });
-  if (client.credentials) {
-      await saveCredentials(client);
-  }
-  return client;
+
+  const code = await getAccessCode(authUrl, redirect_uris[0]);
+  const { tokens } = await oAuth2Client.getToken(code);
+  oAuth2Client.setCredentials(tokens);
+
+  // Guarda el token de usuario, igual no guardarlo nunca o guardarlo en la base de datos NUNCA en vdad está bien porque asi no guardamos informacion sobre el usuario
+  // fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens));
+  // console.log('Token stored to', TOKEN_PATH);
+
+  return oAuth2Client;
+}
+
+async function getAccessCode(authUrl, redirectUri) {
+  const { default: open } = await import('open');
+  return new Promise((resolve, reject) => {
+    const oauthRouter = express();
+
+    oauthRouter.get('/oauthGoogle', (req, res) => {
+      const code = req.query.code;
+      res.send('¡Se ha incluido tu horario a tu google calendar! Puedes cerrar esta pestaña.');
+      server.close(() => {
+        resolve(code);
+      });
+    });
+
+    const server = oauthRouter.listen(3000, () => {
+      open(authUrl).catch(err => {
+        console.error('Error opening URL:', err);
+        reject(err);
+      });
+    });
+  });
 }
 
 async function createEvent(auth, userId) {
+  const eventIds = await getEventIds(userId);
+  if (eventIds !== null) await deleteEventsBatch(auth, eventIds);
 
-  const eventIds = await getEventIds(userId)
-  if (eventIds !== null)  await deleteEventsBatch(auth, eventIds)
-  
   try {
-    
-    const events = await helpers.formatEventsForGoogleCalendar(userId)
+    const events = await helpers.formatEventsForGoogleCalendar(userId);
     return createEventsBatch(auth, events);
-
-    //para eventos individuales--------
-    // console.log(events,'events')
-    //   const response = await calendar.events.insert({
-    //       calendarId: 'primary',
-    //       resource: events,
-    //   });
-    //   console.log('Event created: %s', response.data.htmlLink);
-    //   return response.data.htmlLink
   } catch (error) {
-      console.error('Error creating event: ', error);
+    console.error('Error creating event: ', error);
   }
 }
 
-//batch of Created EVENTS
+// Batch of created EVENTS
 async function createEventsBatch(auth, events) {
-  // const calendar = google.calendar({ version: 'v3', auth });
   const url = 'https://www.googleapis.com/batch/calendar/v3';
   const boundary = 'batch_calendar_boundary';
   let batchBody = '';
@@ -97,7 +91,6 @@ async function createEventsBatch(auth, events) {
         'Content-Type': `multipart/mixed; boundary=${boundary}`,
         Authorization: `Bearer ${auth.credentials.access_token}`,
       },
-     
     });
 
     const eventIds = [];
@@ -107,16 +100,14 @@ async function createEventsBatch(auth, events) {
     while ((match = regex.exec(response.data)) !== null) {
       eventIds.push(match[1]);
     }
-    // console.log(response.data)
-
+     // console.log(response.data)
     return eventIds;
-   
   } catch (error) {
     console.error('Error creating events in batch: ', error.response ? error.response.data : error.message);
   }
 }
 
-//DELETE borra los eventos creados anteriormente guardados en la base de datos en un BATCH
+// DELETE previously created events saved in the database in a BATCH
 async function deleteEventsBatch(auth, eventIds) {
   const url = 'https://www.googleapis.com/batch/calendar/v3';
   const boundary = 'batch_calendar_boundary';
@@ -138,26 +129,25 @@ async function deleteEventsBatch(auth, eventIds) {
         Authorization: `Bearer ${auth.credentials.access_token}`,
       },
     });
-    // console.log('Batch delete response: ', response.data);
+
+        // console.log('Batch delete response: ', response.data);
   } catch (error) {
     console.error('Error deleting events in batch: ', error.response ? error.response.data : error.message);
   }
 }
 
-async function guardarEventIds (eventIds,user_id) {
-  await knex('t_horarios').update({calendar_event_ids : `${JSON.stringify(eventIds)}`}).where({user_id})
+async function guardarEventIds(eventIds, user_id) {
+  await knex('t_horarios').update({ calendar_event_ids: JSON.stringify(eventIds) }).where({ user_id });
 }
 
-async function getEventIds (user_id) {
-  const eventIds = await knex('t_horarios').first('calendar_event_ids').where({user_id})
-  return eventIds.calendar_event_ids
+async function getEventIds(user_id) {
+  const eventIds = await knex('t_horarios').first('calendar_event_ids').where({ user_id });
+  return eventIds.calendar_event_ids;
 }
 
-
-function authentification (userId){
-  return authorize().then( auth => createEvent(auth,userId))
-  .then(eventIds => guardarEventIds(eventIds, userId)).catch(console.error);
+function authentification(userId) {
+  return authorize().then(auth => createEvent(auth, userId))
+    .then(eventIds => guardarEventIds(eventIds, userId)).catch(console.error);
 }
 
-module.exports = {authentification}
-
+module.exports = { authentification };
